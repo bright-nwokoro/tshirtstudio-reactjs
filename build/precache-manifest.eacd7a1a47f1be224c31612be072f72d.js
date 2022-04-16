ac0eed28f91f8b0cd6a6db1df50960b6a0a5a631
Bright Nwokoro <42393834+bright-nwokoro@users.noreply.github.com>
2022-04-16 09:37:07 +0100

    Build React JS App

---
+++ build/precache-manifest.eacd7a1a47f1be224c31612be072f72d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d0134605b3d0fd56b39",
+    "url": "/static/css/main.2800dc3d.chunk.css"
+  },
+  {
+    "revision": "4d0134605b3d0fd56b39",
+    "url": "/static/js/main.4d013460.chunk.js"
+  },
+  {
+    "revision": "6c43dafbfe8de6417706",
+    "url": "/static/js/1.6c43dafb.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a72cca5bed62f31e89bbe064f4c8ae3d",
+    "url": "/index.html"
+  }
+];
